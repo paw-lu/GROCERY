65ef0a19ebe8b62a7129159b6049b687b4741d67
Paulo Costa <PauloSCosta5@gmail.com>
2018-07-12 09:10:25 -0700

    Added playground area, figured out js issues in MDC

---
+++ Playground/app.js
@@ -0,0 +1,12 @@
+import {MDCSelect} from '@material/select';
+import {MDCTextField} from '@material/textfield';
+import {MDCFormField} from '@material/form-field';
+import {MDCCheckbox} from '@material/checkbox';
+import {MDCRipple} from '@material/ripple';
+
+
+
+
+
+const fabRipple2 = new MDCRipple(document.querySelector('.app-fab--absolute'));
+const fabRipple = new MDCRipple(document.querySelector('.mdc-button--raised'));
